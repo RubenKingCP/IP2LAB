abb9c2c48bbb1e70f564433d00fcb0e13319b0fa
RubenKingCP <kostaslaiosedu@gmail.com>
2023-11-01 17:42:06 +0200

    FInish minMax V2

---
+++ WEEK 5/MinAndMax/minAndMaxV2/min-max-V2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   
+
+    int size; //Variable to store the total amount of numbers
+
+    //Get total of numbers
+    printf("Enter amount of numbers: ");
+    scanf("%d", &size);
+
+    //Dynamic allocate memory in array
+    int *array = (int *)malloc(size * sizeof(int));
+
+    //for loop to get numbers
+    for (int i = 0; i < size; i++) {
+        //Enter numbers to array
+        printf("Enter number: ");
+        scanf("%d", &array[i]);
+    }
+
+
+    //Set the basemark
+    int min = array[0];
+    int max = array[0];
+
+    //For loop to itirate through the array
+    for (int i = 0; i < size; i++) {
+        //Check if number is larger than max
+        if (array[i] > max) max = array[i];
+
+        //Check if number is smaller than min
+        else if (array[i] < min) min = array[i];
+    }
+
+    //Print min and max
+    printf("Min: %d\nMax: %d", min, max);
+    return 0;
+}
