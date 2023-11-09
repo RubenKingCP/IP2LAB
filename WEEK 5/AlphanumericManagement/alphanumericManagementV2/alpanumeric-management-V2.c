ed473e7126d8f51a0e8597c6005b511e1a5a97e8
Kostas Laios <105710011+RubenKingCP@users.noreply.github.com>
2023-11-09 16:15:39 +0200

    Do the  v3 of the alphanumeric app

--- WEEK 5/AlphanumericManagement/alphanumericManagementV2/alpanumeric-management-V2.c
+++ WEEK 5/AlphanumericManagement/alphanumericManagementV2/alpanumeric-management-V2.c
@@ -3,40 +3,117 @@
 #include <string.h>
 #define N 10
 
+//Enumaration
+enum methods {
+    	NORMAL,
+    	LETTERS,
+    	LINES,
+    	REMOVESTART,
+    	REMOVEEND,
+    	REVERSEREMOVE,
+    	EXIT
+	};
+
+//Prototypes
+void normal(char * name);
+
+void letterByLetter(char * name, int size);
+
+void lineByLine(char * name, int size);
+
+void removeLettersStart(char * name, int size);
+
+void removeLettersEnd(char * name, int size);
+
+void reverseRemoveLettersEnd(char * name, int size);
 
 
 int main()
 {
-    
     //Set variables
     char name[N];
+    enum methods method;
 
     //Ask name
     printf("Enter your surname: ");
-    scanf("%10s", name);
+    fgets(name, sizeof(name), stdin);
 
     int size = strlen(name);
-   
-    //Question a
-    printf("Question A: ");
-    printf("%s\n", name);   //Print statement to print the name
-
-    //Question b
-    printf("\nQuestion B: ");
-    for (int i = 0; i <=  size; i++) printf("%c ", name[i]);    //For loop to print letter by letter EDIT: Added a space in the print to make the result cleaner  
-
-    //Question c
-    printf("\n\nQuestion C: \n");
-    for (int i = 0; i <  size; i++) printf("%c\n", name[i]);   //Normal for loop to print letter by letter and new line 
-
-    //Question d
-    printf("\nQuestion D: \n");
-    for (int i = 0; i <  size; i++) printf("%s\n", &name[i]);  //With the &string[i] you will print from the specified position and after      
-    
-    //Question e
-    printf("\nQuestion E: \n");
-    for (int i = size; i > 0; i--) printf("%.*s\n",i, name);   //Using * u can set a variable for that number and with a negative counting for loop we get the desired result
-
-
+   	
+   	while (method != 6) {
+	   	printf("Chooce method:\nNormal: 0\nLetter by letter: 1\nLine by line: 2\nRemove first letter each time: 3\nRemove last letter each time: 4\nReverse remove first letter: 5\nEXIT: 6\n");
+	   	scanf("%d", &method);
+	   	
+	   	switch(method){
+	   		case NORMAL:
+	 			normal(name);
+	    		break;
+	    	case LETTERS:
+	   			letterByLetter(name, size);
+	   			break;
+	   		case LINES:
+	   			lineByLine(name, size);
+    			break;
+	    	case REMOVESTART:
+	    		removeLettersStart(name, size);
+	   			break;
+	    	case REMOVEEND:
+	   			removeLettersEnd(name, size);
+	   			break;
+	    	case REVERSEREMOVE:
+	   			reverseRemoveLettersEnd(name, size);
+	   			break;
+	   		case EXIT:
+	   			printf("Shutting down...");
+	   			sleep(1);
+    			break;
+    		default:
+    			printf("Error!");
+    			break;
+		    }		
+   	 	}
     return 0;
 }
+
+void normal(char * name) {
+	printf("%s\n", name);   //Print statement to print the name
+}
+
+void letterByLetter(char * name, int size) {
+	int i;
+	for (i = 0; i <=  size; i++) printf("%c ", name[i]);    //For loop to print letter by letter EDIT: Added a space in the print to make the result cleaner
+	printf("\n");
+}
+
+void lineByLine(char * name, int size) {
+	int i;
+	for (i = 0; i <  size; i++) printf("%c\n", name[i]);   //Normal for loop to print letter by letter and new line 
+}
+
+void removeLettersStart(char * name, int size) {
+	int i;
+	for (i = 0; i <  size; i++) printf("%s\n", &name[i]);  //With the &string[i] you will print from the specified position and after
+}
+
+void removeLettersEnd(char * name, int size) {
+	int i;
+	for (i = size; i > 0; i--) printf("%.*s\n",i, name);   //Using * u can set a variable for that number and with a negative counting for loop we get the desired result
+
+}
+
+void reverseRemoveLettersEnd(char * name, int size) {
+	char reversed[N];
+	int i;
+	//Reverse string
+	while (name[i] != '\0') {
+		reversed[i] = name[size - 1 - i];
+		i++;
+	}
+	
+	//Do the change using the reversed string
+	removeLettersEnd(reversed, size);
+}
+
+
+
+//If word over 10 letters it has a problem
---
+++ WEEK 6/Fractions/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "headers/i2p.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	readExpression("*", 1, 3, 4, 5);
+	return 0;
+}
