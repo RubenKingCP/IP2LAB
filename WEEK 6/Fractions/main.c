ecaf9ba3df50049c5846c0ed1620667827b470c1
Kostas Laios <105710011+RubenKingCP@users.noreply.github.com>
2023-11-23 15:03:22 +0200

    Update code cuz didnt do it in a while
    Many changes in the word soritng and the fractions program as well as added the descriptions

--- WEEK 6/Fractions/main.c
+++ WEEK 6/Fractions/main.c
@@ -1,7 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "headers/i2p.h"
+
+/*FUNCTION main
+A.1 Declare variables: operatorExpr, num1, num2, num3, num4, numOfNumbers, i
+A.2 Display the message "How many times: "
+A.3 Get the number of iterations (numOfNumbers)
+A.4 Loop for i < numOfNumbers with a step of 1
+A.4.1 Call the function readExpression to get the expression parameters
+A.4.2 Use the value of operatorExpr to perform the appropriate arithmetic operation
+A.4.2.1 Call the functions add, sub, multi, or divide based on the operator
+A.5 End the program
+
+FUNCTION add
+B.1 Declare variables: upper, base
+B.2 Calculate the common denominator (base)
+B.3 Calculate the products of the numerators (num1 * num4, num3 * num2)
+B.4 Add the multiplied numerators
+B.5 Call the simplify function with the parameters upper, base
+
+FUNCTION sub
+C.1 Declare variables: upper, base
+C.2 Calculate the common denominator (base)
+C.3 Calculate the products of the numerators (num1 * num4, num3 * num2)
+C.4 Subtract the multiplied numerators
+C.5 Call the simplify function with the parameters pper, base
+
+FUNCTION multi
+D.1 Declare variables: upper, base
+D.2 Calculate the common denominator (base)
+D.3 Calculate the product of the numerators (num1 * num3)
+D.4 Call the simplify function with the parameters upper, base
+
+FUNCTION divide
+E.1 Declare variables: upper, base
+E.2 Calculate the common denominator (base)
+E.3 Calculate the product of the numerators (num1 * num4)
+E.4 Call the simplify function with the upper, base
+
+FUNCTION simplify
+F.1 Declare variable: i
+F.2 Loop for each i from 1 to base with a step of 1
+F.2.1 Check if i divides both numerators
+F.2.1.1 Divide the numerators by i
+F.2.1.2 Set i to 2 and repeat the check
+F.3 Display the simplified expression based on the case
+*/
 
 //St prototypes
 void add(int num1, int num2, int num3, int num4);
@@ -12,7 +56,7 @@ void multi(int num1, int num2, int num3, int num4);
 
 void divide(int num1, int num2, int num3, int num4);
 
-void simplify(char expression, int upper, int base);
+void simplify(int upper, int base);
 
 int main() {
 	//Variables
@@ -57,8 +101,6 @@ int main() {
 void add(int num1, int num2, int num3, int num4) {
 	//Set variables
 	int upper, base;
-	//Give the expression charachter
-	char expression = '+';
 	//Get the same base
 	base = num2 * num4;
 	//Multiply the upper numbers
@@ -66,14 +108,12 @@ void add(int num1, int num2, int num3, int num4) {
 	num3 = num3 * num2;
 	//Add them
 	upper = num1 + num3;
-	simplify(expression, upper, base);
+	simplify(upper, base);
 };
 
 void sub(int num1, int num2, int num3, int num4) {
 	//Set variables
 	int upper, base;
-	//Give the expression charachter
-	char expression = '-';
 	//Get the same base
 	base = num2 * num4;
 	//Multiply the upper numbers
@@ -82,48 +122,63 @@ void sub(int num1, int num2, int num3, int num4) {
 	//Sub them
 	upper = num1 - num3;
 	//Simplify
-	simplify(expression, upper, base);
+	simplify(upper, base);
 }
 
 void multi(int num1, int num2, int num3, int num4) {
 	//Set variables
 	int base, upper;
-	//Give charachter charachter
-	char expression = '*';
 	//Get same base
 	base = num2 * num4;
 	//Get upper number
 	upper = num1 * num3;
 	//Simplify
-	simplify(expression, upper, base);
+	simplify(upper, base);
 }
 
 void divide(int num1, int num2, int num3, int num4){
 	//Set variables
 	int base, upper;
-	//Give the expression charachter
-	char expression = '/';
 	//Get same base
 	base = num2 * num3;
 	//Get upper number
 	upper = num1 * num4;
-	simplify(expression, upper, base);
+	simplify(upper, base);
 }
 
-void simplify(char expression, int upper, int base) {
+void simplify(int upper, int base) {
 	//Set i variable
-	int i;
+	int i, num = 0;
 	//Loop for the base
-	for (i = 1; i <= base; i++ ) {
+	for (i = 2; i <= base; i++ ) {
 		//Check to see if i divides both numbers
 		if (upper % i == 0 && base % i == 0) {
 			//Divide the numbers
 			upper = upper / i;
 			base = base / i;
-			//Set i to 1 and repeat
-			i = 1;
+			//Set i to 2 and repeat
+			i = 2;
 		}
 	}
-	//Print the numbers
-	printf("%c %d/%d\n", expression, upper, base);
+	
+	//Check case and print the matching case
+	if (upper == base) {
+		printf("\t1\n");
+	} 
+	//Check if upper > base
+	else if (upper > base) {
+		do {
+			//Remove from the upper the base and 1 to number
+			num++;
+			upper -= base;
+		} while (upper >= base);
+		//If there is no leftover
+		if (upper == 0) {
+			printf("\t%d\n", num);
+		} else printf("\t%d %d/%d", num, upper, base);
+	}
+	else {
+		//Print the numbers
+		printf("\t%d/%d\n", upper, base);
+	}
 }
--- WEEK 6/WordSorting/main.c
+++ WEEK 6/WordSorting/main.c
@@ -2,6 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* Perigrafi
+FUNCTION main
+A.1 Declare variables
+A.2 Request the first line of words and allocate necessary space
+A.3 Ask the user for their choice
+A.4 Perform the corresponding action
+A.5 Terminate the program
+
+FUNCTION allocateMemory
+B.1 Declare variables
+B.2 Get the number of words
+B.3 Assign the corresponding space
+
+FUNCTION getWords
+C.1 Declare variables
+C.2 Get the sequence of words
+
+FUNCTION printWords
+D.1 Declare variables
+D.2 Display the sequence of words
+
+FUNCTION getWordsStats
+E.1 Declare variables
+E.2 Initialize variable values
+E.3 Compare words
+E.4 Display the longest, shortest, and average word
+
+FUNCTION getCharachterStats
+F.1 Declare variables
+F.2 Get a character
+F.3 Initialize variable values
+F.4 Find the number of occurrences
+F.5 Find maximum and minimum occurrences and store the word where they occur
+F.6 Find the average occurrences
+F.7 Display maximum, minimum, average occurrences, and the words with maximum and minimum occurrences
+
+FUNCTION freeMemory
+G.1 Declare variables
+G.2 Free up space
+*/
+
 //Set prototypes
 void allocateMemory(int *size, char *** words);
 
@@ -9,9 +50,11 @@ void getWords(int size, char ** words);
 
 void printWords(int size, char ** words);
 
-void getStats(int size, char ** words);
+void getWordStats(int size, char ** words);
 
 void getCharacterStats(int size, char ** words);
+
+void freeMemory(int size, char ** words);
 
 //Set enumaration
 enum menu {
@@ -28,6 +71,12 @@ int main() {
 	char ** words;
 	int choice = -1;
 	
+	//Get the first word
+	allocateMemory(&size, &words);
+	getWords(size, words);
+	
+	system("cls");
+	
 	while(choice != TERMINATE) {
 		//Ask for choice
 		printf("Choose action: \nTERMINATE: 0\nENTER WORDS: 1\nPRINT WORDS: 2\nGET WORDS STATS: 3\nGET CHARACTER STATS: 4\n");
@@ -38,35 +87,39 @@ int main() {
 		
 		switch(choice) {
 			case TERMINATE:
-				//Free the allocated memory once program is done
-				for (word = 0; word < size; word++) {
-					free(words[word]);
-				}
-				free(words);
-				exit(0);
+				freeMemory(size, words);
+				printf("Exiting program...!");
+				return 0;
+				break;
 			case GET_WORDS:
+				//Free previous allocated memory
+				freeMemory(size, words);
 				//Get size and give memory
 				allocateMemory(&size, &words);
 				//Get words function
 				getWords(size, words);
+				//System to clear screen
 				system("pause");
 				system("cls");
 				break;
 			case SHOW_WORDS:
 				//Print the word list
 				printWords(size, words);
+				//System to clear screen
 				system("pause");
 				system("cls");
 				break;
 			case WORD_STATS:
 				//Get the words status
-				getStats(size, words);
+				getWordStats(size, words);
+				//System to clear screen
 				system("pause");
 				system("cls");
 				break;
 			case CHARACTER_STATS:
 				//Get character status
 				getCharacterStats(size, words);
+				//System to clear screen
 				system("pause");
 				system("cls");
 				break;
@@ -80,10 +133,15 @@ int main() {
 void allocateMemory(int *size, char *** words) {
 	//Set variables
 	int i;
-	//Ask number of words
-	printf("Enter how many words you want(up to 30): ");
-	scanf("%d", size);
 	
+	//Repeat till under 30 words
+	do {
+		//Ask number of words
+		printf("Enter how many words you want(up to 30): ");
+		scanf("%d", size);
+	}
+	while(*size > 30 || *size == 0);
+	 
 	//Check to see the size
 	if (*size > 30) {
 		printf("Word limit error!\nExiting program.");
@@ -120,7 +178,7 @@ void printWords(int size, char ** words) {
 	printf("\n");
 }
 
-void getStats(int size, char ** words) {
+void getWordStats(int size, char ** words) {
 	//Set variables;
 	int word, min, max, wordLen;
 	float average;
@@ -158,7 +216,7 @@ void getCharacterStats(int size, char ** words) {
 	
 	//Set starting values
 	charMax = 0;
-	charMin = 100;
+	charMin = 900; //30 words * 30 charachters maximum appearances
 	charAverage = 0;
 	charTotal = 0;
 	
@@ -189,4 +247,14 @@ void getCharacterStats(int size, char ** words) {
 	charAverage = (float)charTotal / (float)size;
 	//Print Stats
 	printf("TotalChar - %d\nAverageChar - %.2f\nMaxChar - %d\nMinChar - %d\nMaxWord - %s\nMinWord - %s\n", charTotal, charAverage, charMax, charMin, words[maxWord], words[minWord]);
+}
+
+void freeMemory(int size, char ** words) {
+	int word;
+	
+	//Free the allocated memory once program is done
+	for (word = 0; word < size; word++) {
+		free(words[word]);
+	}
+	free(words);
 }
