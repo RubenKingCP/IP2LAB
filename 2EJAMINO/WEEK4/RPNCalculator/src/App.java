6dd32636c396dc7aff23e2cf27fc3591dbe314eb
RubenKingCP <kostaslaiosedu@gmail.com>
2024-03-15 18:25:46 +0200

    Completed the system to check for inputtYPE

--- 2EJAMINO/WEEK4/RPNCalculator/src/App.java
+++ 2EJAMINO/WEEK4/RPNCalculator/src/App.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class App {
+    static final int OPERAND = 1;
     static String expression;
     static Scanner sc;
+    static Stack<Integer> stack;
+    static String operand;
+    static int inputType;
 
     static String getExpression() {
         sc = new Scanner(System.in);
@@ -11,12 +16,22 @@ public class App {
         return s;
     }
 
+    private static int getOp(String input){
+        if(Character.isDigit(input.charAt(0))){
+            return 1;
+        } 
+        return input.charAt(0);
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        expression = getExpression();
+        sc = new Scanner(System.in);
+        System.out.println("Rpn Caclulator");
+        while(sc.hasNext()){
+            var s = sc.next();
+            inputType = getOp(s);
+            System.out.println("Rpn expression is : " + s + " with type: " + inputType);
+        }
         
-        System.out.println("Rpn expression is : " + expression);
-        
-        // Close the Scanner object after you're done using it
+
+        sc.close();
     }
 }
