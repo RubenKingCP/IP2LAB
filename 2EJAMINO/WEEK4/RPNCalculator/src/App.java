e072764d5f92e04edece77c2c6aa6ca02d4e9623
RubenKingCP <kostaslaiosedu@gmail.com>
2024-03-15 19:01:37 +0200

    Finish Rpn Calculator 1.0.0

--- 2EJAMINO/WEEK4/RPNCalculator/src/App.java
+++ 2EJAMINO/WEEK4/RPNCalculator/src/App.java
@@ -3,35 +3,91 @@ import java.util.Stack;
 
 public class App {
     static final int OPERAND = 1;
-    static String expression;
     static Scanner sc;
-    static Stack<Integer> stack;
-    static String operand;
-    static int inputType;
+    static Stack<Double> stack;
 
     static String getExpression() {
         sc = new Scanner(System.in);
-        System.out.print("Expression to evaluate:");
-        var s = sc.nextLine();
-        return s;
+        System.out.print("Expression to evaluate: ");
+        return sc.nextLine();
     }
 
-    private static int getOp(String input){
-        if(Character.isDigit(input.charAt(0))){
-            return 1;
-        } 
+    private static int getInputType(String input) {
+        if (Character.isDigit(input.charAt(0)) || input.charAt(0) == '.') {
+            return OPERAND;
+        }
         return input.charAt(0);
     }
-    public static void main(String[] args) throws Exception {
-        sc = new Scanner(System.in);
-        System.out.println("Rpn Caclulator");
-        while(sc.hasNext()){
-            var s = sc.next();
-            inputType = getOp(s);
-            System.out.println("Rpn expression is : " + s + " with type: " + inputType);
-        }
-        
 
-        sc.close();
+    static void pushOperand(String s) {
+        stack.push(Double.parseDouble(s));
+    }
+
+    private static void performAddition() {
+        stack.push(stack.pop() + stack.pop());
+    }
+
+    private static void performSubtraction() {
+        double subtrahend = stack.pop();
+        double minuend = stack.pop();
+        stack.push(minuend - subtrahend);
+    }
+
+    private static void performMultiplication() {
+        stack.push(stack.pop() * stack.pop());
+    }
+
+    private static void performDivision() {
+        double divisor = stack.pop();
+        double numerator = stack.pop();
+
+        if (divisor == 0) {
+            stack.push(numerator);
+            stack.push(divisor);
+            return;
+        }
+        stack.push(numerator / divisor);
+    }
+
+    private static void printResult() {
+        System.out.println("Result = " + stack.peek());
+    }
+
+    public static void main(String[] args) {
+        stack = new Stack<>();
+        System.out.println("RPN Calculator");
+
+        String expression = getExpression();
+        Scanner inputScanner = new Scanner(expression);
+
+        while (inputScanner.hasNext()) {
+            String token = inputScanner.next();
+            int inputType = getInputType(token);
+
+            switch (inputType) {
+                case OPERAND:
+                    pushOperand(token);
+                    break;
+                case '+':
+                    performAddition();
+                    break;
+                case '-':
+                    performSubtraction();
+                    break;
+                case '*':
+                    performMultiplication();
+                    break;
+                case '/':
+                    performDivision();
+                    break;
+                case '=':
+                    printResult();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        inputScanner.close();
     }
 }
