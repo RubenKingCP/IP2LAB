679422367baa85c83b5f4c168f80f051d740313f
RubenKingCP <kostaslaiosedu@gmail.com>
2024-04-11 17:38:24 +0300

    Finish version 2 (used the code from the jars)

--- 2EJAMINO/WEEK7/RpnGuiV2/src/App.java
+++ 2EJAMINO/WEEK7/RpnGuiV2/src/App.java
@@ -1,5 +1,11 @@
+import java.util.Stack;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Stack<Double> st = new Stack<>();
+        Operand op = new Operand(st);
+        new CalculatorGui(op);
+        
     }
 }
