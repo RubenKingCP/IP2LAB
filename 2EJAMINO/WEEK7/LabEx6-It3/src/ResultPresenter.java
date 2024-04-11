2435e9950ce75a2f57179eabcd71c366ceaa329c
RubenKingCP <kostaslaiosedu@gmail.com>
2024-04-11 17:54:11 +0300

    Copy paste for the win

---
+++ 2EJAMINO/WEEK7/LabEx6-It3/src/ResultPresenter.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class ResultPresenter {
+    Stack<Double> st;
+  
+    public ResultPresenter(Stack<Double> st) {
+      this.st = st;
+    }
+    
+    public void operate() {
+      String str = ((Double)this.st.pop()).toString();
+      System.out.println(str);
+    }
+}
---
+++ 2EJAMINO/WEEK7/LabEx6-It3/src/Subtracter.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class Subtracter {
+  Stack<Double> st;
+  
+  public Subtracter(Stack<Double> st) {
+    this.st = st;
+  }
+  
+  public void operate() {
+    Double d = this.st.pop();
+    this.st.push(Double.valueOf(((Double)this.st.pop()).doubleValue() - d.doubleValue()));
+  }
+}
